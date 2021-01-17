9b83e3e38b613b7f7a8a81d126f4c7d3650c1b5b
Marcin Chudy <marcinchudy94@gmail.com>
2021-01-18 00:21:33 +0100

    Add demo example

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,48 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import { loadStylesFromFigma } from './figma';
+import styled, { ThemeProvider } from 'styled-components';
+import { loadColorsFromFigma } from './figma';
 import Loader from './Loader';
+import photo from './assets/images/photo.png';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
+  const [colors, setColors] = useState<Record<string, string>>();
 
   useEffect(() => {
-    loadStylesFromFigma().then(() => setIsLoading(false));
+    const loadColors = async () => {
+      const colors = await loadColorsFromFigma();
+      setColors(colors);
+      setIsLoading(false);
+    };
+
+    loadColors();
   }, []);
+
+  console.log(colors);
 
   return (
     <Container>
       {isLoading && <Loader />}
-      {!isLoading && <div>Loaded</div>}
+      {!isLoading && (
+        <ThemeProvider theme={{ colors }}>
+          <CardContainer>
+            <Card>
+              <Content>
+                <LeftContent>
+                  <div>
+                    <Header>Summer Sale</Header>
+                    <Description>
+                      25% Off Now through sunday for all in-store purchases.
+                    </Description>
+                  </div>
+                  <Button>Shop Now</Button>
+                </LeftContent>
+                <img src={photo} alt="Olive leaves" />
+              </Content>
+            </Card>
+          </CardContainer>
+        </ThemeProvider>
+      )}
     </Container>
   );
 }
@@ -24,6 +53,58 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  background: #eeeeee;
+`;
+
+const CardContainer = styled.div`
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+`;
+
+const Card = styled.div`
+  background: #ffffff;
+  border-radius: 32px;
+  padding: 64px;
+  filter: drop-shadow(0px 8px 16px rgba(17, 17, 17, 0.04));
+  max-width: 840px;
+`;
+
+const Content = styled.div`
+  display: flex;
+`;
+
+const LeftContent = styled.div`
+  padding-right: 90px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const Header = styled.div`
+  font-size: 48px;
+  letter-spacing: 1px;
+  font-weight: 700;
+  line-height: 50px;
+  margin-bottom: 20px;
+  color: ${(props) => props.theme.colors['Grayscale / Title-Active']};
+`;
+
+const Description = styled.div`
+  font-size: 24px;
+  line-height: 38px;
+  letter-spacing: 0.75px;
+  color: ${(props) => props.theme.colors['Grayscale / Title-Active']};
+`;
+
+const Button = styled.button`
+  border-radius: 40px;
+  border-width: 0;
+  width: 160px;
+  height: 64px;
+  font-size: 16px;
+  background-color: ${(props) => props.theme.colors['Primary/Default']};
+  color: #fff;
+  font-weight: 600;
+  letter-spacing: 0.75px;
 `;
 
 export default App;
--- src/figma.ts
+++ src/figma.ts
@@ -1,25 +1,74 @@
-import { Client } from 'figma-js';
-
-console.log(process.env);
+import { Canvas, Client, Frame } from 'figma-js';
 
 const client = Client({
-  // Never expose the token in a publicly-hosted app!
+  // For localhost only, never expose the token in a publicly-hosted app!
   personalAccessToken: process.env.REACT_APP_FIGMA_TOKEN,
 });
 
 const fileId = process.env.REACT_APP_FIGMA_FILE!;
 
-export async function loadStylesFromFigma() {
+export async function loadColorsFromFigma(): Promise<Record<string, string>> {
   const file = await client.file(fileId);
-  console.log(file.data.styles);
 
-  const branding = file.data.document.children.find(
-    (c) => c.name === '🎨 Branding'
-  );
-  console.log('branding', branding);
+  const brandingCanvas = file.data.document.children.find(
+    (c) => c.name === '🎨 Branding' && c.type === 'CANVAS'
+  ) as Canvas;
 
-  const styles = await client.fileNodes(fileId, {
-    ids: ['1:448'],
-  });
-  console.log(styles);
+  const colorsFrame = brandingCanvas.children.find(
+    (node) => node.type === 'FRAME' && node.name === 'Colors'
+  ) as Frame;
+
+  const colorStyleDefinitions = Object.entries(file.data.styles)
+    .filter(([_, style]) => style.styleType === 'FILL')
+    .map(([id, style]) => ({ id, name: style.name }));
+
+  return colorsFrame.children
+    .map((node) => {
+      if (node.type !== 'RECTANGLE') {
+        return null;
+      }
+
+      if (!node.styles || !('fill' in node.styles)) {
+        return null;
+      }
+
+      const styles = node.styles;
+      const styleDefinition = colorStyleDefinitions.find(
+        (definition) => definition.id === styles.fill
+      );
+      if (!styleDefinition) {
+        return null;
+      }
+
+      const color = node.fills[0].color;
+      if (!color) {
+        return null;
+      }
+
+      return {
+        name: styleDefinition.name,
+        color: rgbToHex(color.r, color.g, color.b),
+      };
+    })
+    .filter(notEmpty)
+    .reduce(
+      (obj, item) => ({
+        ...obj,
+        [item.name]: item.color,
+      }),
+      {}
+    );
+}
+
+function componentToHex(c: number) {
+  const hex = Math.round(c * 255).toString(16);
+  return hex.length === 1 ? '0' + hex : hex;
+}
+
+function rgbToHex(r: number, g: number, b: number) {
+  return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function notEmpty<TValue>(value: TValue | null | undefined): value is TValue {
+  return value !== null && value !== undefined;
 }
